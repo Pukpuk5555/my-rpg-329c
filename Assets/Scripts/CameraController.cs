ab4535167bb52112d732cf6f960bb53314d12dc4
Pukkkkk <pukpukmakinggame1111@gmail.com>
2025-01-25 15:24:23 +0700

    Finish Basic Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    [Header("Move")]
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float xInput;
+    [SerializeField] private float zInput;
+
+    public static CameraController instance;
+
+    private void Awake()
+    {
+        instance = this;
+        cam = Camera.main;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveSpeed = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveByKB();
+    }
+
+    private void MoveByKB()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
+        transform.position += dir * moveSpeed * Time.deltaTime;
+    }
+}
