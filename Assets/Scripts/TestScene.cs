ceeb56909f890d116ec3ede214208b449b01ea14
Pukkkkk <pukpukmakinggame1111@gmail.com>
2025-02-01 16:16:52 +0700

    Test Scene

---
+++ Assets/Scripts/TestScene.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class TestScene : MonoBehaviour
+{
+    [SerializeField] private Character[] character;
+
+    public void SetIdle()
+    {
+        for(int i = 0; i < character.Length; i++)
+        {
+            character[i].SetState(CharState.Idle);
+        }
+    }
+
+    public void SetWalk()
+    {
+        for (int i = 0; i < character.Length; i++)
+            character[i].SetState(CharState.Walk);
+    }
+
+    public void SetAttack()
+    {
+        for(int i = 0; i < character.Length; i++)
+        {
+            character[i].SetState(CharState.Attack);
+            character[i].Anim.SetTrigger("Attack");
+        }
+    }
+
+    public void SetDie()
+    {
+        for(int i = 0; i < character.Length; i++)
+        {
+            character[i].SetState(CharState.Die);
+            character[i].Anim.SetTrigger("Die");
+        }
+    }
+}
