2da1cb4f9d328b0e216f5e429ea9f4c04eeb0265
Pukkkkk <pukpukmakinggame1111@gmail.com>
2025-03-15 15:12:12 +0700

    Load & Shoot Magic

--- Assets/Scripts/VFXManager.cs
+++ Assets/Scripts/VFXManager.cs
@@ -5,17 +5,47 @@ public class VFXManager : MonoBehaviour
     [SerializeField] private GameObject doubleRingMarker;
     public GameObject DoubleRingMarker {get { return doubleRingMarker; } }
 
+    [SerializeField]
+    private GameObject[] magicVFX;
+    public GameObject[] MagicVFX { get { return magicVFX; } }
+
     public static VFXManager instance;
-    
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        instance = this;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadMagic(int id, Vector3 posA, float time)
+    {
+        //Load Magic
+        if (magicVFX[id] == null)
+            return;
+
+        GameObject objLoad = Instantiate(magicVFX[id], posA, Quaternion.identity);
+        Destroy(objLoad, time);
+    }
+
+    public void ShootMagic(int id, Vector3 posA, Vector3 posB, float time)
+    {
+        //Shoot Magic
+        if (magicVFX[id] == null)
+            return;
+
+        GameObject objShoot = Instantiate(magicVFX[id], posA, Quaternion.identity);
+        objShoot.transform.position = Vector3.LerpUnclamped(posA, posB, time);
+        Destroy(objShoot, time);
     }
 }
