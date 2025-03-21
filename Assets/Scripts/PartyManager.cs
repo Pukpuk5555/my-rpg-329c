3f53fb832b411bc6fe8ce978dc3af9c99ecc6717
Pukkkkk <pukpukmakinggame1111@gmail.com>
2025-03-21 23:42:21 +0700

    Create MagicSO FireBall & PowerGlow

--- Assets/Scripts/PartyManager.cs
+++ Assets/Scripts/PartyManager.cs
@@ -28,8 +28,8 @@ public class PartyManager : MonoBehaviour
 
         SelectSingleHero(0);
 
-        members[0].MagicSkills.Add(new Magic(0, "Power Glow", 10f, 20, 3f, 1f, 2, 2));
-        members[1].MagicSkills.Add(new Magic(0, "Fire Ball", 10f, 35, 3f, 4f, 0, 1));
+        members[0].MagicSkills.Add(new Magic(VFXManager.instance.MagicData[0]));
+        members[1].MagicSkills.Add(new Magic(VFXManager.instance.MagicData[1]));
 
         UIManager.instance.ShowMagicToggles();
     }
--- Assets/Scripts/VFXManager.cs
+++ Assets/Scripts/VFXManager.cs
@@ -9,6 +9,10 @@ public class VFXManager : MonoBehaviour
     private GameObject[] magicVFX;
     public GameObject[] MagicVFX { get { return magicVFX; } }
 
+    [SerializeField]
+    private MagicData[] magicData;
+    public MagicData[] MagicData { get { return magicData; } }
+
     public static VFXManager instance;
 
     private void Awake()
