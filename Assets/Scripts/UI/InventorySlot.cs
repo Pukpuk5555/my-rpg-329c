6c80a04d023a68a16af49ff07d8d5a164f299d15
Pukkkkk <pukpukmakinggame1111@gmail.com>
2025-03-29 17:51:16 +0700

    OnDrop item

--- Assets/Scripts/UI/InventorySlot.cs
+++ Assets/Scripts/UI/InventorySlot.cs
@@ -3,13 +3,43 @@ using UnityEngine.EventSystems;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+    [SerializeField]
+    private int id;
+    public int ID
+    { get { return id; } set { id = value; } }
+
+    [SerializeField]
+    private InventoryManager inventoryManager;
+
+    private void Start()
+    {
+        inventoryManager = InventoryManager.instance;
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
-        if (transform.childCount > 0)
-            return;
+        //Get Item A
+        GameObject objA = eventData.pointerDrag;
+        ItemDrag itemDragA = objA.GetComponent<ItemDrag>();
+        InventorySlot slotA = itemDragA.IconParent.GetComponent<InventorySlot>();
 
-        GameObject objDrop = eventData.pointerDrag;
-        ItemDrag item = objDrop.GetComponent<ItemDrag>();
-        item.IconParent = transform;
+        //Remove Item A from slot A
+        inventoryManager.RemoveItemInBag(slotA.ID);
+
+        //There is an Item B in Slot B
+        if (transform.childCount > 0)
+        {
+            GameObject objB = transform.GetChild(0).gameObject;
+            ItemDrag itemDragB = objB.GetComponent<ItemDrag>();
+
+            //Set Item B on Slot A
+            itemDragB.transform.SetParent(itemDragA.IconParent);
+            itemDragB.IconParent = itemDragA.IconParent;
+            inventoryManager.SaveItemInBag(slotA.ID, itemDragB.Item);
+        }
+
+        //Set Item A on Slot A
+        itemDragA.IconParent = transform;
+        inventoryManager.SaveItemInBag(id, itemDragA.Item);
     }
 }
